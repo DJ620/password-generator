0779172fbcd3ced18bf3dc92d5dde1da232c1770
DJ Hersh <DanielJHersh@gmail.com>
2020-10-27 21:27:28 -0700

    recommented and reorganized the code

--- script.js
+++ script.js
@@ -1,42 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// String of the alphabet split into an array of each individual letter
+
+// Declaring the character sets for the user to choose from
 var letters = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperLetters = letters.map(a=>a.toUpperCase());
-console.log(upperLetters);
-console.log(letters);
-// Array of numbers from 0 to 9
 var numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-// String of special characters split into an array of each individual character
 var characters = "!@#$%^&*()+=~[]{}/?.,<>;:".split("");
+
 // The function that will generate the password
 function generatePassword() {
   // Declaring my result as an empty array
   var result=[];
-  var random;
-  // Variable that stores the length of the password based on user input
-  var pwLength = prompt("How many characters would you like in the password?");
-  // A while statement to check that the length selected is between 8 and 128
-  while (pwLength < 8 || pwLength > 128) {
-    // If the length selected is not within the parameters, a prompt lets the user know to keep the length between 8 and 128
-    pwLength = prompt("Password must be a minimum of 8 characters and no larger than 128 characters.");
-  }
-  // Declaring the variables to hold boolean values of whether the user wants to include certain elements in their password
-  var lowercase;
-  var uppercase;
-  var numbers;
-  var specialCharacters;
+  // Declaring an empty array that will hold character sets that the user chooses later on
+  var criteria = [];
+
   // A while statement to make sure that the user selects at least one of the presented criteria
   while (!lowercase && !uppercase && !numbers && !specialCharacters) {
-    lowercase = confirm("Would you like to include lowercase letters?");
-    uppercase = confirm("Would you like to include uppercase letters?");
-    numbers = confirm("Would you like to include numbers?");
-    specialCharacters = confirm("Would you like to include special characters?");
+    var lowercase = confirm("Would you like to include lowercase letters?");
+    var uppercase = confirm("Would you like to include uppercase letters?");
+    var numbers = confirm("Would you like to include numbers?");
+    var specialCharacters = confirm("Would you like to include special characters?");
+    // If the user says no to all criteria, they are alerted that they must choose at least one, and the loop is repeated
     if (!lowercase && !uppercase && !numbers && !specialCharacters) {
       alert("Please select at least one parameter");
     }
   }
-  var criteria = [];
+
+  // Depending on which of the criteria the user wants to include, that specific character set is added to the criteria array
   if (lowercase) {
     criteria.push(letters);
   }
@@ -50,18 +40,33 @@ function generatePassword() {
     criteria.push(characters);
   }
 
-  // A function that selects a random element from whatever array is passed through it
-  function randomPick(array) {
-    return array[Math.floor(Math.random() * array.length)];
+  // Variable that stores the length of the password based on user input
+  var pwLength = prompt("How many characters would you like in your password?");
+
+  // A loop that will run if the length selected is not between 8 and 128
+  while (pwLength < 8 || pwLength > 128) {
+    // If the length selected is not within the parameters, a prompt lets the user know to keep the length between 8 and 128 and asks the user to once again enter the length of their desired password
+    alert("Password must be between 8 and 128 characters.");
+    pwLength = prompt("How many characters would you like in your password?");
   }
-  // A while statement that runs until the password is the length specified by the user
+
+  // A loop to guarantee that at least one character from each selected criteria is included in the generated password
+  for (let i=0; i<criteria.length; i++) {
+    result.push(criteria[i][Math.floor(Math.random() * criteria[i].length)]);
+  }
+
+  // A while statement that runs until the password is the length specified by the user. This loop will randomize the order of types of characters
   while (result.length < pwLength) {
-    random = criteria[Math.floor(Math.random() * criteria.length)];
+    // Sets random to a randomly selected character set within the criteria array
+    var random = criteria[Math.floor(Math.random() * criteria.length)];
+    // Adds a random index from the pre-determined random array to the result array
     result.push(random[Math.floor(Math.random() * random.length)]);
   }
+
   // Returns the result converted into a string
   return result.join('');
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
