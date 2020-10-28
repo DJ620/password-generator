2df83f342ee3cd757c5380b64cc07e1007b28c34
DJ Hersh <DanielJHersh@gmail.com>
2020-10-27 20:38:32 -0700

    rewrote the algorithm to randomize order of characters

--- copy.js
+++ copy.js
@@ -2,9 +2,6 @@
 var generateBtn = document.querySelector("#generate");
 // String of the alphabet split into an array of each individual letter
 var letters = "abcdefghijklmnopqrstuvwxyz".split("");
-var upperLetters = letters.map(a=>a.toUpperCase());
-console.log(upperLetters);
-console.log(letters);
 // Array of numbers from 0 to 9
 var numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 // String of special characters split into an array of each individual character
@@ -13,7 +10,6 @@ var characters = "!@#$%^&*()+=~[]{}/?.,<>;:".split("");
 function generatePassword() {
   // Declaring my result as an empty array
   var result=[];
-  var random;
   // Variable that stores the length of the password based on user input
   var pwLength = prompt("How many characters would you like in the password?");
   // A while statement to check that the length selected is between 8 and 128
@@ -32,32 +28,29 @@ function generatePassword() {
     uppercase = confirm("Would you like to include uppercase letters?");
     numbers = confirm("Would you like to include numbers?");
     specialCharacters = confirm("Would you like to include special characters?");
-    if (!lowercase && !uppercase && !numbers && !specialCharacters) {
-      alert("Please select at least one parameter");
-    }
   }
-  var criteria = [];
-  if (lowercase) {
-    criteria.push(letters);
-  }
-  if (uppercase) {
-    criteria.push(upperLetters);
-  }
-  if (numbers) {
-    criteria.push(numbersArray);
-  }
-  if (specialCharacters) {
-    criteria.push(characters);
-  }
-
   // A function that selects a random element from whatever array is passed through it
   function randomPick(array) {
     return array[Math.floor(Math.random() * array.length)];
   }
   // A while statement that runs until the password is the length specified by the user
   while (result.length < pwLength) {
-    random = criteria[Math.floor(Math.random() * criteria.length)];
-    result.push(random[Math.floor(Math.random() * random.length)]);
+    // If the user indicated they would like to include lowercase characters, they are added here
+    if (lowercase) {
+      result.push(randomPick(letters));
+    }
+    // If the user indicated they would like to include uppercase characters, they are added here
+    if (uppercase) {
+      result.push(randomPick(letters).toUpperCase());
+    }
+    // If the user indicated they would like to include numbers, they are added here
+    if (numbers) {
+      result.push(randomPick(numbersArray));
+    }
+    // If the user indicated they would like to include special characters, they are added here
+    if (specialCharacters) {
+      result.push(randomPick(characters));
+    }
   }
   // Returns the result converted into a string
   return result.join('');
@@ -68,6 +61,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 }
 
 // Add event listener to generate button
